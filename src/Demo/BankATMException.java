6a8550c9e588f133f90b4bd6520b5be8fe6ae7c1
MithunBurman7 <mithunburman7@gmail.com>
2017-07-03 16:29:14 +0530

    Assignmnet12.4

---
+++ src/Demo/BankATMException.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class BankATMException extends Exception {
+
+	
+	public BankATMException(){
+		 // it will call the super constructor of parent class and display the message 
+		 super("You can not withdraw the amount || your account balance is below 10000")
+			 ;
+	 
+	 } 
+}
